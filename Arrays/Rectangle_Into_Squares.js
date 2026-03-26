26345b54d1e65687dcfd070d5cafb0a8efdad897
FuzSn3aky <acczaepicgames1@gmail.com>
2026-03-26 23:13:50 +0100

    minor fix

--- Arrays/Rectangle_Into_Squares.js
+++ Arrays/Rectangle_Into_Squares.js
@@ -1,20 +1,5 @@
 
 
-//EXAMPLE 13 * 4 = 52
-// DIVIDENT(52) / DIVISOR SQUARED(4*4) = MATH.FLOOR(Quotient) PUSH IN SQUARE ARRAY QUOTIENT NUMBER OF TIMES SIDE
-//RESULT FOR NOW : 52/16 = 3.25 so it goes 3(4*4) + ...
-// NOW SUBTRACT DIVIDENT AND DIVISOR * QUOTIENT 52-48 = 4
-// DIVISOR SQUARED NOW DIVIDE WITH SUBTRACT RESULT 16/4 = 4
-// NOW THAT CHANGE IS 0 IT'S DONE
-
-
-//Formula i made in LaTeX:
-
-//\displaystyle\sum_{Q\neq 0}\left(\sum_{j = 0}^{\left\lfloor Q \right\rfloor -1}
-//d  \quad;\quad D \leftarrow D_{before} - \left \lfloor Q \right \rfloor \cdot d^2 \right)\\
-//D = length \cdot width \\
-//d = (length < width) \quad ? \quad length:width \\
-//Q = \frac {D}{d^2}
 
 
 
